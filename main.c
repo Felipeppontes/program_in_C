fb4b089db8662df9e12f7ee6b709c50869104aa7
Felipe Pontes <107514967+Felipeppontes@users.noreply.github.com>
2022-12-17 00:46:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,32 @@
+// ***********MÉDIA DAS NOTAS-SIMPLES***********
+
+//MÉDIAS DAS MINHAS NOTAS NA UNIVERSIDADE
+
 #include <stdio.h>
 
 int main(){
-    float A,B,C,media_f;
-    printf("\n Por favor, digite suas 3 notas em ordem das unidades:\nPrimeira Unidade:");
-    scanf("%f", &A);
+    float f1,f2,f3,media_f;
+    printf("\n Por favor, digite suas 3 notas em ordem das unidades:\n\nPrimeira Unidade:");
+    scanf("%f", &f1);
     printf("\nSegunda Unidade:");
-    scanf("%f", &B);
+    scanf("%f", &f2);
     printf("\nTerceira Unidade:");
-    scanf("%f", &C);
+    scanf("%f", &f3);
 
-    media_f=(A+B+C)/3;
+    media_f=(f1+f2+f3)/3;
 
     if (media_f >= 7.00)
         {
         printf("\nEsta aprovado!");
         }
+    else if (media_f < 7.00 && media_f >= 3){
+        printf("\nUfa! Você vai ter uma ultima chance. Prova Final!");
+    }
     else
         {
-        printf("\nEsta reprovado!");
+        printf("\nLamento mas você está reprovado(a)!");
         }
-
-    printf("\n\nA media das notas foi: %.2f", media_f);
-
+    printf("\nA média das suas três notas (1ª %.2f |2ª %.2f |3ª %.2f) foi: %.2f", f1, f2, f3, media_f);
 
     return 0;
 }
